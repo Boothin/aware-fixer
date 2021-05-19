bfb602a1951bf0659550682f6a93be314f22764b
Boothin <jeffp@jeffpham.com>
2021-05-19 02:42:33 -0400

    Create blank plugin

---
+++ src/main/java/me/jeeeeef/plugintemplate/PluginTemplate.java
@@ -0,0 +1,15 @@
+package me.jeeeeef.plugintemplate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginTemplate extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // TODO Insert logic to be performed when the plugin is enabled
+    }
+    
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
+}
